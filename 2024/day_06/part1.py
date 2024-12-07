99d38189a3b40b1a1338ef247d142552dc9bdf1d
Gaurav12058 <rothkumar12@gmail.com>
2024-12-07 14:51:10 -0800

    d6 p1

---
+++ 2024/day_06/part1.py
@@ -0,0 +1,52 @@
+def turnRight(direction):
+    return (direction + 1) % 4
+
+def moveBack(i, j, direction):
+    if(direction == 0):
+        i += 1
+    elif(direction == 2):
+        i -= 1
+    elif(direction == 1):
+        j -= 1
+    elif(direction == 3):
+        j += 1
+    return i, j
+
+def moveForward(i, j, direction):
+    if(direction == 0):
+        i -= 1
+    elif(direction == 2):
+        i += 1
+    elif(direction == 1):
+        j += 1
+    elif(direction == 3):
+        j -= 1
+    return i, j
+
+map = []
+
+direction = 0
+with open("input", "r") as file:
+    for line in file:
+        line = line.strip()
+        map.append(line)
+        if "^" in line:
+            i = map.index(line)
+            j = line.index("^")
+
+
+spaces = 0
+visited = set()
+
+while(j < len(map[0]) and i < len(map) and i >= 0 and j >= 0):
+    
+    if((map[i][j] == "." or map[i][j] == "^") and not (i,j) in visited):
+        spaces += 1
+    
+    visited.add((i,j))
+    if(map[i][j] == "#"):
+        i, j = moveBack(i, j, direction)
+        direction = turnRight(direction)
+    i, j = moveForward(i, j, direction)
+
+print(spaces)
